569e9f202e5e2f84ffdbbf6eb42aaef4fe202af1
Dhia Bendjedia <dhia.bendjedia20@gmail.com>
2024-08-23 23:32:53 +0100

    Add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
 
@@ -8,6 +9,7 @@ function App() {
       <main className="overflow-x-hidden">
         <Hero />
       </main>
+      <Footer />
     </div>
   );
 }
